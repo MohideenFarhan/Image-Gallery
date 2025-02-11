9393d0b187e26b7365a7c2d87d7e115ed39b065b
MohideenFarhan <mohideenfarhan12345@gmail.com>
2025-02-11 16:07:41 +0530

    Made changes in style.css,index.html,script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,50 @@
-const images = document.querySelectorAll(".items img"); 
+const gallery = document.querySelector(".items"); 
+
 const lightbox = document.createElement("div");
 const lightboxImg = document.createElement("img");
 const prevBtn = document.createElement("div");
 const nextBtn = document.createElement("div");
 
 lightbox.classList.add("lightbox");
-prevBtn.classList.add("fa-solid", "fa-arrow-left", "lightbox-prev");
-nextBtn.classList.add("fa-solid", "fa-arrow-right", "lightbox-next");
+prevBtn.classList.add( "lightbox-prev");
+nextBtn.classList.add("lightbox-next");
 
 lightbox.append(lightboxImg, prevBtn, nextBtn);
 document.body.appendChild(lightbox);
 
+let images=[]
 let index = 0;
+
+fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then((data) =>{
+        images=data.map((product) => product.image);
+
+        images.forEach((src,i)=>{
+           const img=document.createElement("img");
+            img.src=src;
+            img.classList.add("gallery-img");
+            img.addEventListener("click",()=>showImage(i));
+            gallery.appendChild(img);
+        });
+
+});
 
 function showImage(n) {
     if (n < 0) index = images.length - 1;
     else if (n >= images.length) index = 0;
     else index = n;
     
-    lightboxImg.src = images[index].src;
-    lightbox.style.display = "block";
+    lightboxImg.src = images[index];
+    lightbox.style.display = "flex"; 
 }
-
-images.forEach((img, i) => img.addEventListener("click", () => showImage(i)));
 
 prevBtn.addEventListener("click", () => showImage(index - 1));
 nextBtn.addEventListener("click", () => showImage(index + 1));
-lightbox.addEventListener("click", (e) => e.target === lightbox && (lightbox.style.display = "none"));
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+      }
+});
+
